753cab8527568720681b5d823fd7c05d75fe2698
Raksha-dev11 <raksha@drupaltechie.com>
2025-09-29 20:20:43 +0530

    deploy

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,28 +1,13 @@
-import dbConnect from "@/backend/config/dbConnect";
+import dbConnect from "@/backend/config/dbConnect"
 import { registerUser } from "@/backend/controllers/authControllers";
-import nc from "next-connect";
-import onError from '@/backend/middlewares/errors';
+import { default as nc } from "next-connect";
+import onError from '@/backend/middlewares/errors'
+
 
 const handler = nc({ onError });
 
-// Connect to database
 dbConnect();
 
-// Middleware to handle CORS
-handler.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // allow all origins
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    // Handle preflight requests
-    return res.status(200).end();
-  }
-
-  next();
-});
-
-// Your existing POST handler
 handler.post(registerUser);
 
 export default handler;
