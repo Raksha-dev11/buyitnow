0aedf44361be6df74017a0ebda5813eb7cf55310
Raksha-dev11 <raksha@drupaltechie.com>
2025-09-29 16:44:48 +0530

    redeploy

--- components/admin/Orders.jsx
+++ components/admin/Orders.jsx
@@ -41,7 +41,7 @@ const Orders = ({ orders }) => {
         </thead>
         <tbody>
           {orders?.orders?.map((order) => (
-            <tr className="bg-white">
+             <tr className="bg-white">
               <td className="px-6 py-2">{order?._id}</td>
               <td className="px-6 py-2">{order?.paymentInfo?.amountPaid}</td>
               <td className="px-6 py-2">{order?.orderStatus}</td>
